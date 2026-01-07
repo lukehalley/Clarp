d5fd9337000ce532e6ffa6e66097c8653d82f4d3
Luke Halley <development@lukehalley.com>
2026-01-07 11:42:59 +0800

    style(logo): add brutalist border treatment and scale mascot
    Add orange border with black outline to Clarp logo SVG for better
    visual pop in header. Scale mascot to 88% for breathing room within frame.

--- website/components/Clarp.tsx
+++ website/components/Clarp.tsx
@@ -25,42 +25,40 @@ export default function Clarp({ size = 64, className = '', animate = false }: Cl
         fill="none"
         xmlns="http://www.w3.org/2000/svg"
         className="w-full h-full"
+        style={{ border: '2px solid #FF6B35', outline: '1px solid #0a0a09' }}
       >
         {/* Terracotta background - the substance */}
         <rect width="256" height="256" fill="#D97757"/>
 
-        {/* CLARP mascot - matches marketing-assets/ticker-icon.svg exactly
-            This is the single source of truth for the logo design
-            Total mascot height: 180px (body 120 + legs 60)
-            Centered: (256-180)/2 = 38 from top
-        */}
+        {/* CLARP mascot - scaled down to 75% and centered for breathing room */}
+        <g transform="translate(128, 128) scale(0.88) translate(-128, -128)">
+          {/* Body void */}
+          <rect x="28" y="38" width="200" height="120" fill="#0a0a09"/>
 
-        {/* Body void */}
-        <rect x="28" y="38" width="200" height="120" fill="#0a0a09"/>
+          {/* Left ear */}
+          <rect x="8" y="66" width="20" height="30" fill="#0a0a09"/>
 
-        {/* Left ear */}
-        <rect x="8" y="66" width="20" height="30" fill="#0a0a09"/>
+          {/* Right ear */}
+          <rect x="228" y="66" width="20" height="30" fill="#0a0a09"/>
 
-        {/* Right ear */}
-        <rect x="228" y="66" width="20" height="30" fill="#0a0a09"/>
+          {/* Left eye - positioned for perfect mirror symmetry */}
+          <rect x="64" y="80" width="32" height="48" fill="#D97757"/>
 
-        {/* Left eye - positioned for perfect mirror symmetry */}
-        <rect x="64" y="80" width="32" height="48" fill="#D97757"/>
+          {/* Right eye - mirror of left: 256 - 64 - 32 = 160 */}
+          <rect x="160" y="80" width="32" height="48" fill="#D97757"/>
 
-        {/* Right eye - mirror of left: 256 - 64 - 32 = 160 */}
-        <rect x="160" y="80" width="32" height="48" fill="#D97757"/>
+          {/* Left leg */}
+          <rect x="40" y="158" width="40" height="60" fill="#0a0a09"/>
 
-        {/* Left leg */}
-        <rect x="40" y="158" width="40" height="60" fill="#0a0a09"/>
+          {/* Middle-left leg */}
+          <rect x="100" y="158" width="30" height="60" fill="#0a0a09"/>
 
-        {/* Middle-left leg */}
-        <rect x="100" y="158" width="30" height="60" fill="#0a0a09"/>
+          {/* Middle-right leg */}
+          <rect x="126" y="158" width="30" height="60" fill="#0a0a09"/>
 
-        {/* Middle-right leg */}
-        <rect x="126" y="158" width="30" height="60" fill="#0a0a09"/>
-
-        {/* Right leg */}
-        <rect x="176" y="158" width="40" height="60" fill="#0a0a09"/>
+          {/* Right leg */}
+          <rect x="176" y="158" width="40" height="60" fill="#0a0a09"/>
+        </g>
       </svg>
     </div>
   );
